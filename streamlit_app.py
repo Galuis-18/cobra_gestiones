70d9c1e0818feebaf703790b6c868684080c2cf3
Luis Rios <luis.h.rios.c@gmail.com>
2025-10-24 15:26:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import io  
+import io 
+import tempfile
+import os
 from docx import Document
 from docx.shared import Inches  
 import warnings
